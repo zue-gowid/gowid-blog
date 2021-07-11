80a2c1cb5d1f53d9905d380d2806c3decf9711a8
zue <zue@kakao.com>
2021-07-11 14:29:47 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Gowid Blog",
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss"],
+};
